e60b51c097c9cdd654da05422f2730835101fb84
glowredman <35727266+glowredman@users.noreply.github.com>
2023-12-27 22:18:21 +0100

    Use proper Tags class

--- src/main/java/com/muxiu1997/sharewhereiam/ShareWhereIAm.java
+++ src/main/java/com/muxiu1997/sharewhereiam/ShareWhereIAm.java
@@ -11,9 +11,9 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(
-        modid = Tags.MODID,
+        modid = "sharewhereiam",
         version = Tags.VERSION,
-        name = Tags.MODNAME,
+        name = "Share Where I am",
         acceptedMinecraftVersions = "[1.7.10]",
         dependencies = "required-after:spongemixins@[1.1.0,);")
 public class ShareWhereIAm {
@@ -21,7 +21,9 @@ public class ShareWhereIAm {
     @SuppressWarnings("unused")
     public static final Logger logger = LogManager.getLogger(Tags.MODID);
 
-    @SidedProxy(clientSide = Tags.GROUPNAME + ".proxy.ClientProxy", serverSide = Tags.GROUPNAME + ".proxy.CommonProxy")
+    @SidedProxy(
+            clientSide = "com.muxiu1997.sharewhereiam.proxy.ClientProxy",
+            serverSide = "com.muxiu1997.sharewhereiam.proxy.CommonProxy")
     public static CommonProxy proxy;
 
     @Mod.EventHandler
